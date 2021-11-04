0d5fe3d6d2b3b0f6b22216d1ac46fe899a7d1d48
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-04 10:10:54 +0330

    the first question of the 2nd series is done

--- series2/series2-java/src/com/zamgo/Main.java
+++ series2/series2-java/src/com/zamgo/Main.java
@@ -1,34 +1,47 @@
 package com.zamgo;
 
-import java.util.*;
+import java.util.Scanner;
 
-public class Main
-{
+public class Main {
+
     private static long[] numbers;
     private static int n;
 
-    public static void main(String[] args)
-    {
-        Scanner cin = new Scanner(System.in);
-        n = cin.nextInt();
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        n = console.nextInt();
         numbers = new long[n];
-        getArray(cin, n, numbers);
+        getArray(console, n, numbers);
         System.out.print(getPermutation());
     }
 
-    private static void getArray(Scanner cin, int n, long[] numbers)
-    {
-        for (int i = 0; i < n; i++)
-        {
+    private static void getArray(Scanner cin, int n, long[] numbers) {
+        for (int i = 0; i < n; i++) {
             numbers[i] = cin.nextInt();
         }
     }
-    private static long getPermutation()
-    {
-        Arrays.sort(numbers);
+    private static void selectionSort(){
+        for (int i = 0; i < numbers.length - 1; i++) {
+            int leastIndex = i;
+            for (int j = i; j < numbers.length; j++) {
+                if (numbers[j] < numbers[leastIndex]){
+                    leastIndex = j;
+                }
+            }
+            swap(leastIndex,i);
+        }
+    }
+
+    private static void swap(int i,int j) {
+        long temp = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = temp;
+    }
+
+    private static long getPermutation() {
+        selectionSort();
         long result = 0;
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++) {
             result += Math.abs(numbers[i] - (i + 1));
         }
         return result;
