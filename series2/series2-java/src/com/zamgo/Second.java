c831d92577791eabb8d6b3398db6f89610d902d6
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-05 14:02:10 +0330

    works but not effeciently

--- series2/series2-java/src/com/zamgo/Second.java
+++ series2/series2-java/src/com/zamgo/Second.java
@@ -16,10 +16,23 @@ public class Second {
 
     private static int boxQuantity() {
         int boxQuantity = 1;
-        mergeSort(numbers,0,numbers.length - 1);
-        for (int i = numbers.length - 1; i > 0 ; i--) {
-            if(numbers[i] == numbers[i - 1]){
-                boxQuantity++;
+        for (int i = 0; i < numbers.length; i++) {
+            int maxIndex = i;
+            for (int j = i; j < numbers.length; j++) {
+                if (numbers[j] > numbers[maxIndex]){
+                    maxIndex = j;
+                }
+            }
+            swap(maxIndex,i);
+            if (i != 0){
+                if(numbers[i] == numbers[i - 1]){
+                    boxQuantity++;
+                    for (int j = i + 1; j < numbers.length; j++) {
+                        if (numbers[j] < numbers[i]){
+                            swap(i,j);
+                        }
+                    }
+                }
             }
         }
         return boxQuantity;
@@ -28,46 +41,6 @@ public class Second {
     private static void getArray(Scanner cin, int n, long[] numbers) {
         for (int i = 0; i < n; i++) {
             numbers[i] = cin.nextInt();
-        }
-    }
-
-    private static void mergeSort(long[] array,int leftIndex,int rightIndex){
-        if (leftIndex < rightIndex) {
-            int middleIndex = (leftIndex + rightIndex) / 2;
-            mergeSort(array,leftIndex,middleIndex);
-            mergeSort(array,middleIndex + 1,rightIndex);
-            merge(array,leftIndex,middleIndex,rightIndex);
-        }
-    }
-
-    private static void merge(long[] array, int leftIndex, int middleIndex, int rightIndex) {
-        long[] leftArray = new long[middleIndex - leftIndex + 1];
-        long[] rightArray = new long[rightIndex - middleIndex];
-
-        for (int i = 0; i < leftArray.length; i++){
-            leftArray[i] = array[leftIndex + i];
-        }
-        for (int i = 0; i < rightArray.length; i++){
-            rightArray[i] = array[middleIndex + i + 1];
-        }
-        int li = 0;
-        int ri = 0;
-
-        for (int i = leftIndex; i < rightIndex + 1; i++) {
-            if (li < leftArray.length && ri < rightArray.length) {
-                if (leftArray[li] < rightArray[ri]) {
-                    array[i] = leftArray[li];
-                    li++;
-                } else {
-                    array[i] = rightArray[ri];
-                    ri++;
-                }
-            } else if (li < leftArray.length) {
-                array[i] = leftArray[li];
-                li++;
-            } else if (ri < rightArray.length) {
-                ri++;
-            }
         }
     }
 
