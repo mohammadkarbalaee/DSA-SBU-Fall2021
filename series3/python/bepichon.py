c34356d497eb8f3e872ba506ae1945f7bebdfcda
z.Eftekhari <kavehahangar0ez@gmail.com>
2021-11-17 11:54:32 +0330

    Change function for quick sorting

--- series3/python/bepichon.py
+++ series3/python/bepichon.py
@@ -1,19 +1,27 @@
-def partition(array, start, end):
-    x = array[end]
-    i = start - 1
-    for j in range(start, end-1) :
-        if array[j] <= x :
-            i = i + 1
-            array[j] , array[i] = array[i], array[j]
-    array[end], array[i+1] = array[i+1], array[end]
-    return i+1
+def partition(array, begin, end):
+    pivot_idx = begin
+    for i in range(begin + 1, end + 1):
+        if array[i] <= array[begin]:
+            pivot_idx += 1
+            array[i], array[pivot_idx] = array[pivot_idx], array[i]
+    array[pivot_idx], array[begin] = array[begin], array[pivot_idx]
+    return pivot_idx
 
-def quick_sort(array, start, end) :
-    if start < end :
-        q = partition(array, start, end)
-        quick_sort(array, start, q-1)
-        quick_sort(array, q+1, end)
-        print(array)
+
+def quick_sort_recursion(array, begin, end):
+    if begin >= end:
+        return
+    pivot_idx = partition(array, begin, end)
+    quick_sort_recursion(array, begin, pivot_idx - 1)
+    quick_sort_recursion(array, pivot_idx + 1, end)
+
+
+def quick_sort(array, begin=0, end=None):
+    if end is None:
+        end = len(array) - 1
+
+
+    return quick_sort_recursion(array, begin, end)
 
 
 screw_ascii_arr = []
@@ -29,8 +37,6 @@ for i in range(0, n):
     ascii_screw = ord(char_screw)
     screw_ascii_arr.append(ascii_screw)
 
-#print(screw)
-
 for i in range(0, n):
     char_vertebrae = input()
     vertebrae_char_arr.append(char_vertebrae)
@@ -38,6 +44,5 @@ for i in range(0, n):
     ascii_vertebrae = ord(char_vertebrae)
     vertebrae_ascii_arr.append(ascii_vertebrae)
 
-#print(vertebrae)
-quick_sort(screw_char_arr,0,n-1)
-#print(screw_char_arr)
+quick_sort(screw_char_arr, 0, n-1)
+print(screw_char_arr)
