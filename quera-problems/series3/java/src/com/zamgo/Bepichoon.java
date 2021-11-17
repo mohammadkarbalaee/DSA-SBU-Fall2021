4aa1d539928c7348ed908884274964ca5d2de8ed
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-17 16:49:49 +0330

    works but char number fails

--- quera-problems/series3/java/src/com/zamgo/Bepichoon.java
+++ quera-problems/series3/java/src/com/zamgo/Bepichoon.java
@@ -1,8 +1,34 @@
 package com.zamgo;
 
-public class Bepichoon {
+import java.util.Scanner;
 
+public class Bepichoon {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int nutsBoltsQuantity = in.nextInt();
+        char[] nutsBolts = new char[2 * nutsBoltsQuantity];
+        for (int i = 0; i < 2 * nutsBoltsQuantity; i++) {
+            nutsBolts[i] = in.next().charAt(0);
+        }
+        for (int i = nutsBoltsQuantity; i < 2 * nutsBoltsQuantity; i++) {
+            for (int j = 1; j < nutsBoltsQuantity; j++) {
+                char key = nutsBolts[i];
+                int beforeProperIndex = i - 1;
+                while (beforeProperIndex > 0 && nutsBolts[beforeProperIndex] > key){
+                    nutsBolts[beforeProperIndex + 1] = nutsBolts[beforeProperIndex];
+                    beforeProperIndex--;
+                }
+                nutsBolts[beforeProperIndex + 1] = key;
+            }
+        }
+        System.out.println(nutsBolts);
+//        for (int i = 0; i < 2 * nutsBoltsQuantity; i += 2) {
+//            System.out.print(nutsBolts[i] + " ");
+//        }
+//        System.out.println();
+//        for (int i = 1; i < 2 * nutsBoltsQuantity; i += 2) {
+//            System.out.print(nutsBolts[i] + " ");
+//        }
 
     }
 }
