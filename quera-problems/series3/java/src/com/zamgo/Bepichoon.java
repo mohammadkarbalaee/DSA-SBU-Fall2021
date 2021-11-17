312efc31db9ab2affcf1c223ff41ec09ac666c2b
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-17 18:54:30 +0330

    the last commit for the 2nd problem before solving the 1st problem

--- quera-problems/series3/java/src/com/zamgo/Bepichoon.java
+++ quera-problems/series3/java/src/com/zamgo/Bepichoon.java
@@ -10,25 +10,35 @@ public class Bepichoon {
         for (int i = 0; i < 2 * nutsBoltsQuantity; i++) {
             nutsBolts[i] = in.next().charAt(0);
         }
+
+        int sortedToIndex = 1;
         for (int i = nutsBoltsQuantity; i < 2 * nutsBoltsQuantity; i++) {
-            for (int j = 1; j < nutsBoltsQuantity; j++) {
+            for (int j = 0; j < nutsBoltsQuantity; j++) {
                 char key = nutsBolts[i];
-                int beforeProperIndex = i - 1;
+                int beforeProperIndex = sortedToIndex - 1;
                 while (beforeProperIndex > 0 && nutsBolts[beforeProperIndex] > key){
                     nutsBolts[beforeProperIndex + 1] = nutsBolts[beforeProperIndex];
                     beforeProperIndex--;
                 }
                 nutsBolts[beforeProperIndex + 1] = key;
+                sortedToIndex++;
             }
         }
-        System.out.println(nutsBolts);
-//        for (int i = 0; i < 2 * nutsBoltsQuantity; i += 2) {
-//            System.out.print(nutsBolts[i] + " ");
-//        }
-//        System.out.println();
-//        for (int i = 1; i < 2 * nutsBoltsQuantity; i += 2) {
-//            System.out.print(nutsBolts[i] + " ");
-//        }
 
+        for (int i = 0; i < 2 * nutsBoltsQuantity; i += 2) {
+            System.out.print(nutsBolts[i] + " ");
+        }
+        System.out.println();
+        for (int i = 1; i < 2 * nutsBoltsQuantity; i += 2) {
+            System.out.print(nutsBolts[i] + " ");
+        }
     }
 }
+
+/*
+the key of solving this problem lies under the partition function in the quick sort algorithm
+so, since the first question is about the partition function, let's solve that first and by the knowledge that
+you gain there, you can certainly solve this problem easier.
+
+this text is meant to be a reminder, so that you will not forget what the idea here was
+ */
