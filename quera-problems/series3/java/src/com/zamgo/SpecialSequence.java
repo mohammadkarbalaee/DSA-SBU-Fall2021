e6cd4341b9fbff7ca4712fd8ec5975cfb6ca0695
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-18 10:14:17 +0330

    14 in the 3rd

--- quera-problems/series3/java/src/com/zamgo/SpecialSequence.java
+++ quera-problems/series3/java/src/com/zamgo/SpecialSequence.java
@@ -9,8 +9,33 @@ public class SpecialSequence {
     int specialSubsequenceLength = in.nextInt();
     int[] array = new int[arrayLength];
     int degree = 0;
-    for (int i = 0; i < arrayLength - specialSubsequenceLength; i++) {
-
+    for (int i = 0; i < arrayLength; i++) {
+      array[i] = in.nextInt();
     }
+    for (int i = 0; i < arrayLength - specialSubsequenceLength; i++) {
+      for (int j = i + 1; j < arrayLength; j++) {
+        boolean biggerFound = false;
+        if (array[j] > array[i]){
+          int basesFound = 1;
+          int newBase = array[j];
+          for (int k = j + 1; k < arrayLength; k++) {
+            if (array[k] > newBase){
+              newBase = array[k];
+              basesFound++;
+              if (basesFound == specialSubsequenceLength){
+                biggerFound = true;
+                break;
+              }
+            } else {
+              break;
+            }
+          }
+        }
+        if (biggerFound){
+          degree += array[i];
+        }
+      }
+    }
+    System.out.println(degree);
   }
 }
