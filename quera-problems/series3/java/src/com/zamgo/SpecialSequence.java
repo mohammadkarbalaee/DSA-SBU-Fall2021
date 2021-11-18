975ba4d7e510fa4ad2bb4a7a9dcb4b5d849e76e2
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-18 09:57:58 +0330

    initialization of the 3rd problem

---
+++ quera-problems/series3/java/src/com/zamgo/SpecialSequence.java
@@ -0,0 +1,16 @@
+package com.zamgo;
+
+import java.util.Scanner;
+
+public class SpecialSequence {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int arrayLength = in.nextInt();
+    int specialSubsequenceLength = in.nextInt();
+    int[] array = new int[arrayLength];
+    int degree = 0;
+    for (int i = 0; i < arrayLength - specialSubsequenceLength; i++) {
+
+    }
+  }
+}
