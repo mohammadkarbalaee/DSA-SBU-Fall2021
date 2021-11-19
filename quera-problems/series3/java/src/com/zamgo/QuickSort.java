3bce0b9e18b539b7a26a9e7acbcfcb3b296cd3f1
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-19 12:30:09 +0330

    this series is donnnneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- quera-problems/series3/java/src/com/zamgo/QuickSort.java
+++ quera-problems/series3/java/src/com/zamgo/QuickSort.java
@@ -4,16 +4,14 @@ import java.util.Scanner;
 
 public class QuickSort {
 
-  public static long[] hoareCounts;
-  public static long[] lomutoCounts;
+  public static int hoareCounts = 0;
+  public static int lomutoCounts = 0;
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
     int testsQuantity = in.nextInt();
     long[][] testArraysLomuto = new long[testsQuantity][];
     long[][] testArraysHoare = new long[testsQuantity][];
-    hoareCounts = new long[testsQuantity];
-    lomutoCounts = new long[testsQuantity];
     for (int i = 0; i < testsQuantity; i++){
       int testArrayLength = in.nextInt();
       long[] testArrayLomuto = new long[testArrayLength];
@@ -23,22 +21,22 @@ public class QuickSort {
         testArrayLomuto[j] = input;
         testArrayHoare[j] = input;
       }
-      lomutoCounts[i] = 0;
-      hoareCounts[i] = 0;
       testArraysLomuto[i] = testArrayLomuto;
       testArraysHoare[i] = testArrayHoare;
     }
     for (int i = 0; i < testsQuantity; i++){
-      System.out.println(chooseEfficient(testArraysLomuto[i],testArraysHoare[i],i));
+      System.out.println(chooseEfficient(testArraysLomuto[i],testArraysHoare[i]));
     }
   }
 
-  public static String chooseEfficient(long[] lomutoTestCase,long[] hoareTestCase,int countIndex){
-    lomutoQuickSort(lomutoTestCase,0,lomutoTestCase.length - 1,countIndex);
-    hoareQuickSort(hoareTestCase,0,hoareTestCase.length - 1,countIndex);
-    if (lomutoCounts[countIndex] > hoareCounts[countIndex]){
+  public static String chooseEfficient(long[] lomutoTestCase,long[] hoareTestCase){
+    lomutoCounts = 0;
+    hoareCounts = 0;
+    lomutoQuickSort(lomutoTestCase,0,lomutoTestCase.length - 1);
+    hoareQuickSort(hoareTestCase,0,hoareTestCase.length - 1);
+    if (lomutoCounts > hoareCounts){
       return "HR";
-    } else if(hoareCounts[countIndex] > lomutoCounts[countIndex]){
+    } else if(hoareCounts > lomutoCounts){
       return "LMT";
     } else {
       return "TIE";
@@ -51,17 +49,14 @@ public class QuickSort {
     int j = endIndex + 1;
 
     while (true) {
-
       do {
         i++;
       }
       while (array[i] < pivot);
-
       do {
         j--;
       }
       while (array[j] > pivot);
-
       if (i >= j) {
         return j;
       } else {
@@ -83,21 +78,21 @@ public class QuickSort {
     return startOfLeftHalf + 1;
   }
 
-  public static void lomutoQuickSort(long[] array,int startIndex, int endIndex,int countIndex){
-    lomutoCounts[countIndex]++;
+  public static void lomutoQuickSort(long[] array,int startIndex, int endIndex){
+    lomutoCounts++;
     if (startIndex < endIndex){
       int previousPivotIndex = lomutoPartition(array,startIndex,endIndex);
-      lomutoQuickSort(array,startIndex,previousPivotIndex - 1,countIndex);
-      lomutoQuickSort(array,previousPivotIndex + 1,endIndex,countIndex);
+      lomutoQuickSort(array,startIndex,previousPivotIndex - 1);
+      lomutoQuickSort(array,previousPivotIndex + 1,endIndex);
     }
   }
 
-  public static void hoareQuickSort(long[] array,int startIndex, int endIndex,int countIndex){
-    hoareCounts[countIndex]++;
+  public static void hoareQuickSort(long[] array,int startIndex, int endIndex){
+    hoareCounts++;
     if (startIndex < endIndex){
       int previousPivotIndex = hoarePartition(array,startIndex,endIndex);
-      hoareQuickSort(array,startIndex,previousPivotIndex,countIndex);
-      hoareQuickSort(array,previousPivotIndex + 1,endIndex,countIndex);
+      hoareQuickSort(array,startIndex,previousPivotIndex);
+      hoareQuickSort(array,previousPivotIndex + 1,endIndex);
     }
   }
 
