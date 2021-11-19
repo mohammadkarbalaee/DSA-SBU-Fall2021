159f4ef9ba75ebcda4df336950e12059901792f6
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-19 12:20:46 +0330

    3rd one is also implemented in java

--- quera-problems/series3/java/src/com/zamgo/Bepichoon.java
+++ quera-problems/series3/java/src/com/zamgo/Bepichoon.java
@@ -1,3 +1,4 @@
+package com.zamgo;
 
 import java.util.Scanner;
 
--- quera-problems/series3/java/src/com/zamgo/SpecialSequence.java
+++ quera-problems/series3/java/src/com/zamgo/SpecialSequence.java
@@ -1,3 +1,4 @@
+package com.zamgo;
 
 import java.util.Scanner;
 
@@ -8,70 +9,40 @@ public class SpecialSequence {
     int specialSubsequenceLength = in.nextInt();
     long[] array = new long[arrayLength];
     long degree = 0;
-    for (int i = 0; i < arrayLength; i++) {
+    for (int i = 0; i < arrayLength; i++){
       array[i] = in.nextInt();
     }
-    for (int i = 0; i < arrayLength - specialSubsequenceLength; i++) {
-      boolean isFound = false;
-      for (int j = i; j < arrayLength; j++){
-        int biggerCounter = 0;
-        if (array[j] > array[i]){
-          biggerCounter++;
-          for (int k = j + 1; k < arrayLength; k++) {
-            if (array[k] > array[i]){
-              biggerCounter++;
-              if (biggerCounter == specialSubsequenceLength){
-                if(k == arrayLength - 1){
-                  degree += array[i];
-                  break;
-                } else {
-                  if (array[k + 1] <= array[i]){
-                    degree += array[i];
-                    break;
-                  }
-                }
-              }
-            } else {
-              break;
-            }
-          }
-        }
-        if (biggerCounter == specialSubsequenceLength){
-          isFound = true;
-          break;
-        }
-      }
-
-      if (!isFound){
-          for (int j = i; j > 0; j--){
-            int biggerCounter = 0;
-            if (array[j] > array[i]){
-              biggerCounter++;
-              for (int k = j - 1; k >= 0; k--) {
-                if (array[k] > array[i]){
-                  biggerCounter++;
-                  if (biggerCounter == specialSubsequenceLength){
-                    if(k == 0){
-                      degree += array[i];
-                      break;
-                    } else {
-                      if (array[k - 1] <= array[i]){
-                        degree += array[i];
-                        break;
-                      }
-                    }
-                  }
-                } else {
-                  break;
-                }
-              }
-            }
-            if (biggerCounter == specialSubsequenceLength){
-              break;
-            }
-          }
-        }
-      }
+    quickSort(array,0,arrayLength - 1);
+    for (int i = 0; i < arrayLength - specialSubsequenceLength; i++){
+      degree += array[i];
+    }
     System.out.println(degree);
+  }
+
+  public static int partition(long[] array,int startIndex, int endIndex){
+    long pivot = array[endIndex];
+    int startOfLeftHalf = startIndex - 1;
+    for (int startOfRightHalf = startIndex; startOfRightHalf < endIndex; startOfRightHalf++) {
+      if (array[startOfRightHalf] <= pivot){
+        startOfLeftHalf++;
+        swap(array,startOfLeftHalf,startOfRightHalf);
+      }
+    }
+    swap(array,startOfLeftHalf + 1,endIndex);
+    return startOfLeftHalf + 1;
+  }
+
+  public static void quickSort(long[] array,int startIndex, int endIndex){
+    if (startIndex < endIndex){
+      int previousPivotIndex = partition(array,startIndex,endIndex);
+      quickSort(array,startIndex,previousPivotIndex - 1);
+      quickSort(array,previousPivotIndex + 1,endIndex);
+    }
+  }
+
+  private static void swap(long[] array,int i,int j){
+    long temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
   }
 }
