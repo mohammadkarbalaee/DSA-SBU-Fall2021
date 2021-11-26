5c9e1db5aeb2587b7da695039fcd3bd8c6dc0135
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-26 16:27:47 +0330

    initialization of the second problem

--- quera-problems/series4/java/src/MergeHeap.java
+++ quera-problems/series4/java/src/MergeHeap.java
@@ -1,5 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MergeHeap {
   public static void main(String[] args) {
+    Scanner input =  new Scanner(System.in);
+    int numberOfSortedArrays = input.nextInt();
+    int[][] sortedArrays = new int[numberOfSortedArrays][];
+    int mergedArrayLength = 0;
+    for (int i = 0; i < numberOfSortedArrays; i++) {
+      int sortedArrayLength = input.nextInt();
+      mergedArrayLength += sortedArrayLength;
+      int[] sortedArray = new int[sortedArrayLength];
+      for (int j = 0; j < sortedArrayLength; j++) {
+        sortedArray[j] = input.nextInt();
+      }
+      sortedArrays[i] = sortedArray;
+    }
 
+    int[] minHeapArray = new int[numberOfSortedArrays];
+    for (int i = 0; i < numberOfSortedArrays; i++) {
+      minHeapArray[i] = sortedArrays[i][0];
+    }
+
+    System.out.println(Arrays.toString(minHeapArray));
+
+    int[] mergedArray = new int[mergedArrayLength];
   }
 }
