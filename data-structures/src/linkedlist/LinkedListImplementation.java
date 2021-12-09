4ac76412d40f6cdab709500fa076c390ca6549bf
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-12-09 12:00:24 +0330

    implemented circular linked list

--- data-structures/src/linkedlist/LinkedListImplementation.java
+++ data-structures/src/linkedlist/LinkedListImplementation.java
@@ -3,6 +3,7 @@ package linkedlist;
 public class LinkedListImplementation {
   public static void main(String[] args) {
     LinkedList<Integer> ourLinkedList = new LinkedList<>();
+    CircularLinkedList<Integer> ourCircularLinkedList = new CircularLinkedList<>();
     Node<Integer> newNode = new Node<>(null,10,null);
     ourLinkedList.insert(newNode);
     ourLinkedList.delete(newNode);
@@ -17,6 +18,21 @@ public class LinkedListImplementation {
     ourLinkedList.insert(new Node<>(null,90,null));
     ourLinkedList.insert(new Node<>(null,100,null));
     ourLinkedList.printAllNodes();
+    System.out.println();
+
+    Node<Integer> newNodeCircular = new Node<>(null,10,null);
+    ourCircularLinkedList.insert(newNodeCircular);
+    ourCircularLinkedList.insert(newNodeCircular);
+    ourCircularLinkedList.insert(new Node<>(null,20,null));
+    ourCircularLinkedList.insert(new Node<>(null,30,null));
+    ourCircularLinkedList.insert(new Node<>(null,40,null));
+    ourCircularLinkedList.insert(new Node<>(null,50,null));
+    ourCircularLinkedList.insert(new Node<>(null,60,null));
+    ourCircularLinkedList.insert(new Node<>(null,70,null));
+    ourCircularLinkedList.insert(new Node<>(null,80,null));
+    ourCircularLinkedList.insert(new Node<>(null,90,null));
+    ourCircularLinkedList.insert(new Node<>(null,100,null));
+    System.out.println(ourCircularLinkedList.search(10).data);
   }
 }
 
@@ -66,6 +82,43 @@ class LinkedList<T>{
   public void printAllNodes(){
     Node<T> walkingHead = this.headNode;
     while (walkingHead != null){
+      System.out.print(walkingHead.data + " ");
+      walkingHead = walkingHead.nextNode;
+    }
+  }
+}
+
+class CircularLinkedList<T> extends LinkedList<T>{
+
+  Node<T> nullNode = new Node<>(null,null,null);
+
+  @Override
+  public Node<T> search(T wantedData){
+    Node<T> wantedNode = this.nullNode.nextNode;
+    while (wantedNode != this.nullNode && wantedNode.data != wantedData){
+      wantedNode = wantedNode.nextNode;
+    }
+    return wantedNode;
+  }
+
+  @Override
+  public void insert(Node<T> newNode){
+    newNode.nextNode = this.nullNode.nextNode;
+    this.nullNode.previousNode = newNode;
+    this.nullNode.nextNode = newNode;
+    newNode.previousNode = this.nullNode;
+  }
+
+  @Override
+  public void delete(Node<T> nodeToDelete){
+    nodeToDelete.previousNode.nextNode = nodeToDelete.nextNode;
+    nodeToDelete.nextNode.previousNode = nodeToDelete.previousNode;
+  }
+
+  @Override
+  public void printAllNodes(){
+    Node<T> walkingHead = this.nullNode.nextNode;
+    while (walkingHead != this.nullNode){
       System.out.print(walkingHead.data + " ");
       walkingHead = walkingHead.nextNode;
     }
