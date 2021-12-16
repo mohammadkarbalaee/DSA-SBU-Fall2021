c49df81b0ac71d7c9ec185af5a39e33eea946409
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-12-16 10:25:36 +0330

    add and get implemented for indexed linkedList

---
+++ quera-problems/series5/java/src/BinaryProblem.java
@@ -0,0 +1,7 @@
+public class BinaryProblem {
+  public static void main(String[] args) {
+
+  }
+}
+
+
--- quera-problems/series5/java/src/TextEditorProblem.java
+++ quera-problems/series5/java/src/TextEditorProblem.java
@@ -1,75 +1,143 @@
-import java.util.LinkedList;
-import java.util.Scanner;
-
 public class TextEditorProblem {
   public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    String text = input.next();
-    TextEditor textEditor = new TextEditor();
-    for (int i = 0; i < text.length(); i++) {
-      textEditor.getNewCharacter(text.charAt(i));
-    }
-    System.out.println(textEditor.getEditedString());
+//    Scanner input = new Scanner(System.in);
+//    String text = input.next();
+//    TextEditor textEditor = new TextEditor();
+//    for (int i = 0; i < text.length(); i++) {
+//      textEditor.getNewCharacter(text.charAt(i));
+//    }
+//    System.out.println(textEditor.getEditedString());
+    LinkedList<Character> myList = new LinkedList<>();
+//    myList.insertAt(new Node<>(null,'a',null));
+//    myList.insertAt(new Node<>(null,'b',null));
+//    myList.insertAt(new Node<>(null,'c',null));
+//    myList.insertAt(new Node<>(null,'d',null));
+//    System.out.println(myList.getNodeAt(0).data);
+    myList.add(0,'a');
+    myList.add(0,'b');
+    myList.add(1,'c');
+    System.out.println(myList.get(0).data);
+    System.out.println(myList.get(1).data);
+    System.out.println(myList.get(2).data);
   }
 }
 
-class TextEditor{
+//class TextEditor{
+//
+//  private LinkedList<Character> editedText;
+//  private int cursor;
+//
+//  public TextEditor(){
+//    this.editedText = new LinkedList<>();
+//    this.cursor = -1;
+//  }
+//
+//  public void getNewCharacter(Character newCharacter){
+//    if (isCommand(newCharacter)){
+//      if (newCharacter.equals('-')){
+//        deleteCharAtCursor();
+//      } else if(newCharacter.equals('>')){
+//        if (this.cursor < (this.editedText.size() - 1)){
+//          this.cursor++;
+//        }
+//      } else if (newCharacter.equals('<')){
+//        if (this.cursor > -1){
+//          this.cursor--;
+//        }
+//      }
+//    } else {
+//      this.cursor++;
+//      insertNewCharacter(newCharacter);
+//    }
+//  }
+//
+//  private void deleteCharAtCursor() {
+//    if (this.cursor != -1){
+//      this.editedText.remove(this.cursor);
+//      this.cursor--;
+//    }
+//  }
+//
+//  private boolean isCommand(Character newCharacter){
+//    if (newCharacter.equals('-')  || newCharacter.equals('>') || newCharacter.equals('<')){
+//      return true;
+//    } else {
+//      return false;
+//    }
+//  }
+//
+//  private void insertNewCharacter(char newCharacter){
+//    this.editedText.add(this.cursor,newCharacter);
+//  }
+//
+//  public String getEditedString(){
+//    StringBuilder finalString = new StringBuilder();
+//    for (int i = 0; i < this.editedText.size(); i++) {
+//      finalString.append(this.editedText.get(i));
+//    }
+//    if (finalString.toString().isEmpty()){
+//      return "-1";
+//    } else {
+//      return finalString.toString();
+//    }
+//  }
+//}
 
-  private LinkedList<Character> editedText;
-  private int cursor;
 
-  public TextEditor(){
-    this.editedText = new LinkedList<>();
-    this.cursor = -1;
+
+class Node<T>{
+  public T data;
+  public Node<T> previousNode;
+  public Node<T> nextNode;
+
+  Node(Node<T> previousNode,T data,Node<T> nextNode){
+    this.previousNode = previousNode;
+    this.data = data;
+    this.nextNode = nextNode;
+  }
+}
+
+class LinkedList<T>{
+  public Node<T> headNode = null;
+  private int length = 0;
+
+  public Node<T> get(int wantedNodeIndex){
+    Node<T> wantedNode = this.headNode;
+    int currentIndex = 0;
+    while (wantedNode != null && wantedNodeIndex != currentIndex){
+      wantedNode = wantedNode.nextNode;
+      currentIndex++;
+    }
+    return wantedNode;
   }
 
-  public void getNewCharacter(Character newCharacter){
-    if (isCommand(newCharacter)){
-      if (newCharacter.equals('-')){
-        deleteCharAtCursor();
-      } else if(newCharacter.equals('>')){
-        if (this.cursor < (this.editedText.size() - 1)){
-          this.cursor++;
-        }
-      } else if (newCharacter.equals('<')){
-        if (this.cursor > -1){
-          this.cursor--;
-        }
+  public void add(int indexToInsertAt,T newData){
+    Node<T> currentNode = this.get(indexToInsertAt);
+    Node<T> newNode = new Node<>(null,newData,null);
+    if (currentNode != null){
+      if (currentNode.previousNode != null){
+        newNode.nextNode = currentNode;
+        newNode.previousNode = currentNode.previousNode;
+        currentNode.previousNode.nextNode = newNode;
+        currentNode.previousNode = newNode;
+      } else {
+        newNode.nextNode = this.headNode;
+        this.headNode.previousNode = newNode;
+        newNode.previousNode = null;
+        this.headNode = newNode;
       }
     } else {
-      this.cursor++;
-      insertNewCharacter(newCharacter);
+      this.headNode = newNode;
     }
+    this.length++;
   }
 
-  private void deleteCharAtCursor() {
-    if (this.cursor != -1){
-      this.editedText.remove(this.cursor);
-      this.cursor--;
-    }
+  public void remove(int indexToRemove){
+
   }
 
-  private boolean isCommand(Character newCharacter){
-    if (newCharacter.equals('-')  || newCharacter.equals('>') || newCharacter.equals('<')){
-      return true;
-    } else {
-      return false;
-    }
+  int length(){
+    return this.length;
   }
 
-  private void insertNewCharacter(char newCharacter){
-    this.editedText.add(this.cursor,newCharacter);
-  }
-
-  public String getEditedString(){
-    StringBuilder finalString = new StringBuilder();
-    for (int i = 0; i < this.editedText.size(); i++) {
-      finalString.append(this.editedText.get(i));
-    }
-    if (finalString.toString().isEmpty()){
-      return "-1";
-    } else {
-      return finalString.toString();
-    }
-  }
 }
