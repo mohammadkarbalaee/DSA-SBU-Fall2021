0d68416ab95e93815e64362620b139d20f8c3a7e
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-12-17 15:13:52 +0330

    some minor changes

--- quera-problems/series5/java/src/MaxSumImplementation.java
+++ quera-problems/series5/java/src/MaxSumImplementation.java
@@ -9,14 +9,14 @@ public class MaxSumImplementation {
     for (int i = 0; i < n; i++) {
       array[i] = input.nextInt();
     }
-    int maximumSum = 0;
+    long maximumSum = 0;
     for (int i = 1; i <= m; i++) {
-      int sum = 0;
+      long sum = 0;
       int stackHead = 0;
       int stackTail = n - 1;
 
       for (int j = 0; j < i; j++) {
-        sum += array[j];
+        sum += array[stackHead];
         stackHead++;
       }
 
